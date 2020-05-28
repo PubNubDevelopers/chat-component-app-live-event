17499bda02050ae1de4d2bbae0d1e5e049a5dd60
Nicolas Fodor <nicolas.fodor@pubnub.com>
2020-05-28 10:21:07 -0700

    Adapted calls to react-pubnub for  Typescript functional components, thanks Are <me@iama.re>! Co-authored-by: Are <me@iama.re>

--- src/components/ChatDemo.tsx
+++ src/components/ChatDemo.tsx
@@ -12,53 +12,53 @@ import { PubNubProvider, usePubNub } from 'pubnub-react';
 // import LELogin from '../components/LELogin';
 
 interface ChatInitProps {
-    skin: string,
-    store: any,
-    client: any,
-   }
+  skin: string,
+  store: any,
+  client: any,
+}
 
 const ChatDemo: React.SFC<ChatInitProps> = (props) => {
-//const ChatDemo:FunctionComponent<{ props?: ChatInitProps }> = ({ props = {} }) => {
-const channels = ['awesomeChannel'];
-const [messages, setMessages] = useState([]);
-const [input, setInput] = useState('');
-useEffect(() => console.log(messages), [messages]);
-useEffect(() => console.log(props), [props]);
-const [time, setTime] = useState<number | null>(null);
-const [error, setError] = useState<string | null>(null);
+  //const ChatDemo:FunctionComponent<{ props?: ChatInitProps }> = ({ props = {} }) => {
+  const channels = ['awesomeChannel'];
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState('');
+  useEffect(() => console.log(messages), [messages]);
+  useEffect(() => console.log(props), [props]);
+  const [time, setTime] = useState<number | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
-//useEffect(() => console.log(props.store.dispatch), [props.store.dispatch]);
-const leaveApplication = () => {
+  //useEffect(() => console.log(props.store.dispatch), [props.store.dispatch]);
+  const leaveApplication = () => {
     // This is required to show the current user leave immediately rather than
     // wating for the timeout period
     //useEffect(() => console.log(props), [props]);
 
-    //props.store.unsubscribeAll();
+    props.client.unsubscribeAll();
   };
 
-//   useEffect(() => {
+  //   useEffect(() => {
 
-//     // Create an scoped async function in the hook
-//     async function adaptFunction() {
-//         await  props.client
-//         .time()
-//         .then(({ timetoken }) => {
-//           setTime(timetoken);
-//         })
-//         .catch((error?: string) => {
-//           setError(error.message);
-//         });
-//       }    // Execute the created function directly
-      
-//      // adaptFunction();
+  //     // Create an scoped async function in the hook
+  //     async function adaptFunction() {
+  //         await  props.client
+  //         .time()
+  //         .then(({ timetoken }) => {
+  //           setTime(timetoken);
+  //         })
+  //         .catch((error?: string) => {
+  //           setError(error.message);
+  //         });
+  //       }    // Execute the created function directly
+
+  //      // adaptFunction();
 
 
-//   }, []);
-useEffect(() => {
+  //   }, []);
+  useEffect(() => {
     // Start listening for messages and events from PubNub
     props.client.addListener(createPubNubListener(props.store.dispatch));
     props.client.addListener(createTypingIndicatorsListener(props.store.dispatch));
-    return  leaveApplication();
+    return leaveApplication();
   }, []);
 
   useEffect(() => {
@@ -71,109 +71,119 @@ useEffect(() => {
     // props.client.getMessage('channel1', (msg) => {
     //     console.log(msg);
     // });
-    
+
     props.client.addListener({
-      message: (messages) => {
-         
-        setMessages([messages]);
+      message: (message) => {
+
+        setMessages(function (messages) {
+          return [...messages, message]
+        });
       },
     });
- 
+
     props.client.subscribe({ channels });
-  }, [messages]);
- 
-//   const sendMessage = useCallback(
 
-//     async message => {
-//       await props.client.publish({
-//         channel: channels[0],
-//         message,
-//       });
- 
-//       setInput('');
-//     }
-//     ,
+    return leaveApplication;
+  }, []);
 
-//     [props.client, setInput]
-//   );
+  const sendMessage = useCallback(
 
-    //return (<div>Test</div>);
+    async message => {
+      await props.client.publish({
+        channel: channels[0],
+        message,
+      });
 
-    return (
-      <div className="App">
-        <header className="App-header">
+      setInput('');
+    },
+
+
+    [props.client, setInput]
+  );
+
+  //return (<div>Test</div>);
+
+  // The problem seems to be that you are trying to put hooks inside a function inside a component
+  // Hooks can only be on the "top-level" of a component, they cannot be nested into ifs or 
+
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div
+          style={{
+            width: '500px',
+            height: '301px',
+            border: '1px solid black',
+          }}
+        >
+          <div style={{ backgroundColor: 'grey' }}>React Chat Example {time}</div>
           <div
             style={{
-              width: '500px',
-              height: '301px',
-              border: '1px solid black',
+              backgroundColor: 'white',
+              height: '260px',
+              overflow: 'scroll',
             }}
           >
-            <div style={{ backgroundColor: 'grey' }}>React Chat Example {time}</div>
-            <div
-              style={{
-                backgroundColor: 'white',
-                height: '260px',
-                overflow: 'scroll',
-              }}
-            >
-              {messages.map((message, messageIndex) => {
-                return (
-                  <div
-                    key={`message-${messageIndex}`}
-                    style={{
-                      display: 'inline-block',
-                      float: 'left',
-                      backgroundColor: '#eee',
-                      color: 'black',
-                      borderRadius: '20px',
-                      margin: '5px',
-                      padding: '8px 15px',
-                    }}
-                  >
-                    {message}
-                  </div>
-                );
-              })}
-            </div>
-            <div
-              style={{
-                display: 'flex',
-                height: '40px',
-                backgroundColor: 'lightgrey',
-              }}
-            >
-              <input
-                type="text"
-                style={{
-                  borderRadius: '5px',
-                  flexGrow: 1,
-                  fontSize: '18px',
-                }}
-                placeholder="Type your message"
-                value={input}
-                onChange={e => setInput(e.target.value)}
-              />
-              <button
-                style={{
-                  backgroundColor: 'blue',
-                  color: 'white',
-                  borderRadius: '5px',
-                  fontSize: '16px',
-                }}
-                onClick={e => {
-                  e.preventDefault();
-                  //sendMessage(input);
-                }}
-              >
-                Send Message
-              </button>
-            </div>
+            {messages.map((message, messageIndex) => {
+              return (
+                <div
+                  key={`message-${messageIndex}`}
+                  style={{
+                    display: 'inline-block',
+                    float: 'left',
+                    backgroundColor: '#eee',
+                    color: 'black',
+                    borderRadius: '20px',
+                    margin: '5px',
+                    padding: '8px 15px',
+                  }}
+                >
+                  {message.message}
+                </div>
+              );
+            })}
           </div>
-        </header>
-      </div>
-    );
-    
+          <div
+            style={{
+              display: 'flex',
+              height: '40px',
+              backgroundColor: 'lightgrey',
+            }}
+          >
+            <input
+              type="text"
+              style={{
+                borderRadius: '5px',
+                flexGrow: 1,
+                fontSize: '18px',
+              }}
+              placeholder="Type your message"
+              value={input}
+              onChange={e => setInput(e.target.value)}
+            />
+            <button
+              style={{
+                backgroundColor: 'blue',
+                color: 'white',
+                borderRadius: '5px',
+                fontSize: '16px',
+              }}
+              onClick={e => {
+                e.preventDefault();
+                sendMessage(input);
+              }}
+            >
+              Send Message
+              </button>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+
 }
 
 
