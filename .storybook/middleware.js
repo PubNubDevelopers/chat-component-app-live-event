c388b0bb0beda8414b599328a23f1a5bc718f79e
Nicolas Fodor <nicolas.fodor@pubnub.com>
2020-07-24 16:25:09 -0700

    attempt to get a map to /images through middleware load.

--- .storybook/middleware.js
+++ .storybook/middleware.js
@@ -1,6 +1,7 @@
-const express = require('express');
+-mconst express = require('express');
 const path = require('path');
 
 module.exports = router => {
+    console.log ("aha")
     router.use('/images', express.static(path.join(__dirname, '..', 'images')));
 };
