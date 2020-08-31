189eb556c57bf93b1e5003bc6000e3e15a31dc87
Nicolas Fodor <nicolas.fodor@pubnub.com>
2020-08-31 16:09:11 -0700

    added code bar for serverless demo

--- webpack.config.js
+++ webpack.config.js
@@ -88,7 +88,7 @@ module.exports = {
     }),
     new WebpackShellPlugin({
       onBuildStart: ['echo "Starting Live Event Demo Server..."'],
-      //onBuildEnd: [`ngrok-qr http -host-header=localhost 8080`]
+      onBuildEnd: [`ngrok-qr http -host-header=localhost 8080`]
     })
   ]
 };
